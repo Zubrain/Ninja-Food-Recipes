5616454475facedd25d535909cd17c34a41c6dad
Chinemerem Jideama <ncjideama1@gmail.com>
2021-12-17 23:30:38 +0100

    flex, media screens, cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Nunito']
+      }
+    },
   },
   plugins: [],
 }
